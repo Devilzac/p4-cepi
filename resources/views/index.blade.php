1a1c2ec502486c20d7a4d8bfc34da4bf4fb4a41c
Rodriguez Mahon <kavanshane@CEPIBASE.INF>
2020-02-28 19:50:18 +0100

    [add] Font awesome icons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,41 +1,60 @@
 @extends('layout.principal')
 
 @section('main')
-
-    <table class="table table-striped table-dark py-4">
-      <thead>
-        <tr>
-          <th scope="col">Titulo</th>
-          <th scope="col">Resumen</th>
-          <th scope="col">No. Paginas</th>
-          <th scope="col">Edicion</th>
-          <th scope="col">Autor</th>
-          <th scope="col">Precio</th>
-          <th scope="col">Editar</th>
-          <th scope="col">Borrar</th>
-        </tr>
-      </thead>
-      <tbody>
-            @if($libros->count())
-                @foreach($libros as $libro)
-                    <tr>
-                        <td>{{ $libro->titulo }}</td>
-                        <td>{{ $libro->resumen }}</td>
-                        <td>{{ $libro->npaginas }}</td>
-                        <td>{{ $libro->nedicion }}</td>
-                        <td>{{ $libro->autor }}</td>
-                        <td>{{ $libro->precio }}</td>
-                        <td>
-                            <a class="btn btn-primary btn-xs" href="{{action('LibroController@edit', $libro->id)}}"></a> 
-                        </td>
-                        <td>{{ $libro->precio }}</td>
-                    </tr>
-                @endforeach
-            @else            
-              <tr>
-                <td>No hay libros disponibles!</td>
-              </tr>
-            @endif
-      </tbody>
-    </table>
+    <div class="card">
+      <div class="card-header">
+          <i class="fas fa-book"></i><b>Lista Libros</b>
+          <div class="btn-group float-right" role="group">
+              <a href="{{route('formulario.create')}}" class="btn btn-info" role="buton">
+                  <i class="fas fa-plus"></i> Añadir Libro
+              </a>
+          </div>
+      </div>
+      <div class="card-body">
+            <table class="table table-striped table-dark py-4">
+              <thead>
+                <tr>
+                  <th scope="col">Titulo</th>
+                  <th scope="col">Resumen</th>
+                  <th scope="col">No. Paginas</th>
+                  <th scope="col">Edicion</th>
+                  <th scope="col">Autor</th>
+                  <th scope="col">Precio</th>
+                  <th scope="col">Editar</th>
+                  <th scope="col">Borrar</th>
+                </tr>
+              </thead>
+              <tbody>
+                    @if($libros->count())
+                        @foreach($libros as $libro)
+                            <tr>
+                                <td>{{ $libro->titulo }}</td>
+                                <td>{{ $libro->resumen }}</td>
+                                <td>{{ $libro->npaginas }}</td>
+                                <td>{{ $libro->nedicion }}</td>
+                                <td>{{ $libro->autor }}</td>
+                                <td>{{ $libro->precio }}</td>
+                                <td>
+                                    <a class="btn btn-primary btn-xs" href="{{action('LibroController@edit', $libro->id)}}">
+                                        <i class="fas fa-edit"></i>
+                                    </a> 
+                                </td>
+                                <td>
+                                    <form action="{{action('LibroController@destroy', $libro->id)}}" method="post">
+                                        {{csrf_field}}
+                                        <input name="_method" type="hidden" value="DELETE">
+                                        <button class="btn btn-danger btn-xs" type="submit">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @else           
+                      <tr>
+                        <td>No hay libros disponibles!</td>
+                      </tr>
+                    @endif
+              </tbody>
+            </table>
 @endsection
